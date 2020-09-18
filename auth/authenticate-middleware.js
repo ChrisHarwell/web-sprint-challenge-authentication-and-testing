1425baa142a6090e931d179db7d13d12d53c6289
Chris Harwell <chrisharwell72@gmail.com>
2020-09-18 12:01:27 -0400

    MVP Complete

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,22 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  // add code here to verify users are logged in
+  const [authType, token] = req.headers.authorization.split(" "); //TODO add the header to the request call using Bearer followed by a space and the token
+  console.log("Token: ", token);
+
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({you: "cant touch this (invalid token"})
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+
+  } else {
+    res.status(401).json({you: "have no power (no token included)"});
+  }
 };
